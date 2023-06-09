eb10cd647385e774e0892c7336841bc274c9ca99
Andre Tassis <124065777+andretassis@users.noreply.github.com>
2023-06-09 15:06:13 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# Consumo de API - ViaCEP
+# Consumo de dados de API - ViaCEP
 
 Projeto desenvolvido para o curso de Formação em Front-End da Alura.
+
+## Visão Geral
+
+O projeto foi desenvolvido utilizando HTML, CSS e JavaScript.
+O objetivo principal foi fazer requisições à API ViaCEP (https://viacep.com.br) no campo CEP do formulário.<br>
+
+Ao preencher o campo corretamente, as informações de endereço, bairro, cidade e UF são preenchidas automaticamente.
+
+Caso o usuário insira um CEP incorreto, o sistema mostrará uma mensagem de erro.
+
--- main.js
+++ main.js
@@ -1,3 +1,36 @@
-var consultaCep = fetch('viacep.com.br/ws/01001000/json/')
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    // faz requisições para a API 
+    try {
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`); // faz a requisição para a API
+        var consultaCepConvert = await consultaCep.json(); // converte a informação para JSON
 
-console.log(consultaCep)
+        // mostra mensagem de erro caso o CEP esteja estruturalmente correto, mas não existe 
+        if (consultaCepConvert.erro) {
+            throw Error('CEP não encontrado!');
+        }
+
+        //encontra os campos pelo id
+        var endereco = document.getElementById('endereco')
+        var bairro = document.getElementById('bairro')
+        var cidade = document.getElementById('cidade')
+        var estado = document.getElementById('estado')
+
+        //substitui os valores por aqueles encontrados na API de acordo com o CEP digitado
+        endereco.value = consultaCepConvert.logradouro
+        bairro.value = consultaCepConvert.bairro
+        cidade.value = consultaCepConvert.localidade
+        estado.value = consultaCepConvert.uf
+
+        return consultaCepConvert;
+        // mostra mensagem de erro caso o CEP esteja estruturalmente incorreto
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido! Verifique os valores digitados.</p>`
+    }
+}
+
+var cep = document.getElementById('cep')
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
+
+buscaEndereco();
